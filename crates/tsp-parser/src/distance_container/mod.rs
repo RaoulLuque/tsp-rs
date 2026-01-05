eb30ed61ea0a23e9fc94b3f61226d584a3c2b128
Raoul Luqué <raoulsluque@gmail.com>
2026-01-05 20:25:44 +0100

    feat: Provide barebones implementation of parsing into symmetric matrix

--- crates/tsp-parser/src/distance_container/mod.rs
+++ crates/tsp-parser/src/distance_container/mod.rs
@@ -1,5 +1,8 @@
-use tsp_core::instance::{InstanceMetadata, distance::Distance};
-
+use tsp_core::instance::{
+    InstanceMetadata,
+    distance::Distance,
+    matrix::{Matrix, MatrixSym},
+};
 
 mod matrix_sym;
 
@@ -9,4 +12,17 @@ pub trait ParseFromTSPLib {
         metadata: &InstanceMetadata,
         distance_function: impl Fn(&PointType, &PointType) -> Distance + Sync + Send + Copy,
     ) -> Self;
+}
+
+impl ParseFromTSPLib for Matrix<Distance> {
+    fn from_node_coord_section<PointType: Sync + Send>(
+        node_data: &Vec<PointType>,
+        metadata: &InstanceMetadata,
+        distance_function: impl Fn(&PointType, &PointType) -> Distance + Sync + Send + Copy,
+    ) -> Self {
+        let sym_matrix =
+            MatrixSym::<Distance>::from_node_coord_section(node_data, metadata, distance_function);
+
+        sym_matrix.to_edge_data_matrix()
+    }
 }
